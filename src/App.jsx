b4a80ee390465ac3b0a66b89fead7a1f352a92ad
Amr Ahmed <amrahmed09951@gmail.com>
2026-09-15 01:58:55 +0300

    Declaring a winner

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   const [squares, setSquares] = useState(Array(9).fill(null))
   const [xIsNext, setXIsNext] = useState(true)
   const handleClick = (i) => {
-    if(squares[i]){
+    if(squares[i] || calculateWinner(squares)){
       return;
     }
     const nextSquares = squares.slice()
@@ -16,8 +16,16 @@ function App() {
     setSquares(nextSquares)
     setXIsNext(!xIsNext)
   }
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner){
+    status = "Winner: " + winner;
+  }else {
+    status = "Next Player: " + (xIsNext? "X":"O");
+  }
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -33,7 +41,6 @@ function App() {
         <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
-      {console.log(squares)}
     </>
   )
 }
@@ -41,5 +48,24 @@ const Square = ({onSquareClick, value}) =>{
   return(
     <button className="square" onClick={onSquareClick}>{value}</button>
   )
+}
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 export default App
